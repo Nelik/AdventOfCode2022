5bf0c352b494ca6cb0e3e40d681bb6010e40b331
nela.kasparova <nela.kasparova@trulioo.com>
2022-12-24 17:37:46 -0800

    Revert "Solution for Day 7."
    This reverts commit d9100c0091d3d540bfb74b393fe4bc2ab569b44f.

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,8 +1,7 @@
-import java.lang.Integer.min
 
 fun main() {
 
-    class TreeNode(var value:Int = 0, val name: String){
+    class TreeNode(val value:Int = 0, val name: String){
         var parent:TreeNode? = null
 
         var children:MutableList<TreeNode> = mutableListOf()
@@ -22,70 +21,19 @@ fun main() {
     }
 
     fun createTree(input: List<String>): TreeNode {
-        val treeRoot = TreeNode(name = "/")
-        var currentNode = treeRoot
-        for (line in input) {
-            if (line[0] == '$') {
-                if (line == "$ cd /") continue
-                if (line == "$ cd ..") {
-                    currentNode = currentNode.parent!!
-                } else if (line.contains("$ cd")) {
-                    val cmdParts = line.split(" ")
-                    currentNode = currentNode.children.find { it.name == cmdParts.last() }!!
-                }
-            } else {
-                if (line.contains("dir ")) {
-                    currentNode.addChild(TreeNode(name = line.split(" ").last()))
-                } else {
-                    val fileParts = line.split(" ")
-                    currentNode.addChild(TreeNode(fileParts.first().toInt(), fileParts.last()))
-                }
-            }
-        }
-
-        return treeRoot
+        return TreeNode(name = "/")
     }
 
-    fun countNodesValue(node: TreeNode): Int {
-        return if (node.value != 0) node.value
-        else {
-            node.value = node.children.sumOf { countNodesValue(it) }
-            node.value
-        }
-    }
-
-    fun getSum(node: TreeNode, maxSize: Int): Int {
-        return if (node.children.isEmpty()) 0
-        else if (node.value > maxSize) 0 + node.children.sumOf { getSum(it, maxSize) }
-        else node.value + node.children.sumOf { getSum(it, maxSize) }
-    }
-
-
-    fun findSum(input: List<String>, largeSize: Int = 100000): Int {
-        val tree = createTree(input)
-        countNodesValue(tree)
-        return getSum(tree, largeSize)
-    }
-
-    fun getDirSize(node: TreeNode, minToDelete: Int, minValue: Int): Int {
-        return if (node.children.isEmpty()) minValue
-        else if (node.value in minToDelete until minValue) min(node.value, node.children.minOf { getDirSize(it, minToDelete, minValue) })
-        else min(minValue, node.children.minOf { getDirSize(it, minToDelete, minValue) })
-    }
-
-    fun findDirSize(input: List<String>, diskSpace: Int = 70000000, neededSpace: Int = 30000000): Int {
-        val tree = createTree(input)
-        countNodesValue(tree)
-        val minToDelete = neededSpace - (diskSpace - tree.value)
-        return getDirSize(tree, minToDelete, tree.value)
+    fun findSum(input: List<String>): Int {
+        return 7
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
-    check(findSum(testInput) == 95437)
-    check(findDirSize(testInput) == 24933642)
+    check(findSum(testInput) == 7)
+//    check(findMarker(testInput, 14) == 19)
 //
-    val input = readInput("Day07")
-    findSum(input).println()
-    findDirSize(input).println()
+//    val input = readStream("Day06")
+//    findMarker(input).println()
+//    findMarker(input, 14).println()
 }
